2de30b5c03f5a2d13d622094c78d2f5b02855e3c
Bhavika <itqorconsulting@gmail.com>
2023-03-10 13:42:06 +0530

    add scanner with border curve

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,29 +1,148 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatefulWidget {
+class QRScannerOverlay extends StatelessWidget {
+  const QRScannerOverlay({Key? key, required this.overlayColour})
+      : super(key: key);
 
-  final String value;
-  final Function() screenClosed;
-  const ProfileScreen({
-    Key? key,
-    required this.value,
-    required this.screenClosed,
-  }) : super(key: key);
+  final Color overlayColour;
 
-  @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
-}
-
-class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Profile'),
+    double scanArea = (MediaQuery.of(context).size.width < 400 ||
+        MediaQuery.of(context).size.height < 400)
+        ? 300.0
+        : 350.0;
+    return Stack(
+      alignment: Alignment.center,
+        children: [
+      ColorFiltered(
+        colorFilter: ColorFilter.mode(
+            overlayColour, BlendMode.srcOut), // This one will create the magic
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  color: Colors.red,
+                  backgroundBlendMode: BlendMode
+                      .dstOut), // This one will handle background + difference out
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                height: scanArea,
+                width: scanArea,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+          ],
         ),
-        body:  Center(
-          child: Text('This is the Profile screen. ${widget.value}'),
+      ),
+      Align(
+        alignment: Alignment.center,
+        child: CustomPaint(
+          foregroundPainter: BorderPainter(),
+          child: SizedBox(
+            width: scanArea + 25,
+            height: scanArea + 25,
+          ),
         ),
+      ),
+    ]);
+  }
+}
+
+// Creates the blue borders
+class BorderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    const width = 4.0;
+    const radius = 20.0;
+    const tRadius = 3 * radius;
+    final rect = Rect.fromLTWH(
+      width,
+      width,
+      size.width - 2 * width,
+      size.height - 2 * width,
+    );
+    final rrect = RRect.fromRectAndRadius(rect, const Radius.circular(radius));
+    const clippingRect0 = Rect.fromLTWH(
+      0,
+      0,
+      tRadius,
+      tRadius,
+    );
+    final clippingRect1 = Rect.fromLTWH(
+      size.width - tRadius,
+      0,
+      tRadius,
+      tRadius,
+    );
+    final clippingRect2 = Rect.fromLTWH(
+      0,
+      size.height - tRadius,
+      tRadius,
+      tRadius,
+    );
+    final clippingRect3 = Rect.fromLTWH(
+      size.width - tRadius,
+      size.height - tRadius,
+      tRadius,
+      tRadius,
+    );
+
+    final path = Path()
+      ..addRect(clippingRect0)
+      ..addRect(clippingRect1)
+      ..addRect(clippingRect2)
+      ..addRect(clippingRect3);
+
+    canvas.clipPath(path);
+    canvas.drawRRect(
+      rrect,
+      Paint()
+        ..color = Colors.blue
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = width,
     );
   }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class BarReaderSize {
+  static double width = 200;
+  static double height = 200;
+}
+
+class OverlayWithHolePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()..color = Colors.black54;
+    canvas.drawPath(
+        Path.combine(
+          PathOperation.difference,
+          Path()..addRect(Rect.fromLTWH(0, 0, size.width, size.height)),
+          Path()
+            ..addOval(Rect.fromCircle(
+                center: Offset(size.width - 44, size.height - 44), radius: 40))
+            ..close(),
+        ),
+        paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+@override
+bool shouldRepaint(CustomPainter oldDelegate) {
+  return false;
 }
--- lib/scanner_screen.dart
+++ lib/scanner_screen.dart
@@ -1,6 +1,7 @@
+
+
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/get.dart';
 import 'package:gim_app/profile_screen.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
@@ -13,104 +14,71 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
-  BarcodeCapture? capture;
+   String? capture;
   MobileScannerController cameraController = MobileScannerController();
-  bool _screenOpened = false;
-
+  MobileScannerArguments? arguments;
 
   @override
   Widget build(BuildContext context) {
-    final double screenHeight = MediaQuery.of(context).size.height;
-    final double containerHeight = screenHeight * 0.5; // 50% of screen height
+    double scanArea = (MediaQuery
+        .of(context)
+        .size
+        .width < 400 ||
+        MediaQuery
+            .of(context)
+            .size
+            .height < 400)
+        ? 400.0
+        : 450.0;
+
     return Scaffold(
-      body: SafeArea(
-        child: Builder(
-          builder: (context) {
-            return  Wrap(
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  height: 200,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [Colors.lightBlue, Colors.blue.shade200],
-                      ),
-                      borderRadius: const BorderRadiusDirectional.only(bottomEnd: Radius.circular(20),bottomStart: Radius.circular(20))
-                  ),
-                  child: const Center(
-                    child: Text('Scan QR Code here',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        )),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 18),
+                  child: Text('Gym App',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 25)),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    height: scanArea,
+                    width: scanArea,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        MobileScanner(
+                            controller: cameraController,
+                            onDetect: (barcodes) {
+                              capture = barcodes.barcodes.first.rawValue;
+                                debugPrint('Barcode found! $capture');
+                            }),
+                        const QRScannerOverlay(overlayColour: Colors.white),
+                      ],
+                    ),
                   ),
                 ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    IconButton(
-                      icon: ValueListenableBuilder(
-                        valueListenable: cameraController.torchState,
-                        builder: (context, state, child) {
-                          switch (state) {
-                            case TorchState.off:
-                              return const Icon(Icons.flash_off, color: Colors.grey);
-                            case TorchState.on:
-                              return const Icon(Icons.flash_on, color: Colors.yellow);
-                          }
-                        },
-                      ),
-                      iconSize: 32.0,
-                      onPressed: () => cameraController.toggleTorch(),
-                    ),
-                    IconButton(
-                      icon: ValueListenableBuilder(
-                        valueListenable: cameraController.cameraFacingState,
-                        builder: (context, state, child) {
-                          switch (state as CameraFacing) {
-                            case CameraFacing.front:
-                              return const Icon(Icons.camera_front);
-                            case CameraFacing.back:
-                              return const Icon(Icons.camera_rear);
-                          }
-                        },
-                      ),
-                      iconSize: 32.0,
-                      onPressed: () => cameraController.switchCamera(),
-                    ),
-                  ],),
-                SizedBox(
-                  height: containerHeight,
-                  child: MobileScanner(
-                    fit: BoxFit.cover,
-                    controller: cameraController,
-                    onDetect: (BarcodeCapture capture) {
-                      setState(() {
-                        if (!_screenOpened) {
-                          this.capture = capture;
-                          _screenOpened = true;
-                          Future.delayed(Duration(seconds: 2));
-                          Get.to(ProfileScreen(screenClosed: _screenWasClosed,
-                            value: capture.barcodes.first.rawValue ??
-                                'Scan something!',));
-                        }
-                      });
-                    },
-                  ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 18),
+                  child: Text('Scan QR Code here',
+                      style: TextStyle(
+                        fontSize: 20,
+                      )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 18),
+                  child: Text('Scan QR Code Value: ${capture ?? ''}',
+                      style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.blue
+                      )),
                 ),
               ],
-            );
-          },
-        ),
-      ),
-
+            )
+        )
     );
-  }
-  void _screenWasClosed() {
-    _screenOpened = false;
   }
 }

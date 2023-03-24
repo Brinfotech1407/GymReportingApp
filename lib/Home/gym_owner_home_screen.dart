c0f821577b180a0a2b0fa707e60dc9f1de4f5e55
Bhavika <itqorconsulting@gmail.com>
2023-03-24 12:00:44 +0530

    gym owner home screen UI changes.

--- lib/Home/gym_owner_home_screen.dart
+++ lib/Home/gym_owner_home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/get_instance.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/get_rx/get_rx.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:gim_app/auth/login_screen.dart';
 import 'package:gim_app/controllers/auth_controller.dart';
@@ -19,6 +20,7 @@ class GymOwnerHomeScreen extends StatefulWidget {
 
 class _GymOwnerHomeScreenState extends State<GymOwnerHomeScreen> {
   final MyCollectionController controller = Get.put(MyCollectionController());
+  RxBool showFilterIcon = true.obs;
 
   @override
   void initState() {
@@ -34,27 +36,51 @@ class _GymOwnerHomeScreenState extends State<GymOwnerHomeScreen> {
           decoration: GymUtils().buildBoxDecoration(),
           child: Column(
             children: [
-              Align(
-                alignment: Alignment.topRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 18),
-                  child: IconButton(
-                      onPressed: () async {
-                        QuickAlert.show(
-                          context: context,
-                          type: QuickAlertType.confirm,
-                          title: '',
-                          cancelBtnText: 'No',
-                          confirmBtnText: 'yes',
-                          text:
-                              "Are you sure you want to log out? Your session will be terminated and you will need to log in again to use the app.",
-                          onConfirmBtnTap: () {
-                            AuthController.instance.logout();
-                            Get.to(() => const LoginScreen());
-                          },
-                        );
-                      },
-                      icon: const Icon(Icons.logout,color: Colors.white,)),
+              Obx(
+                () => Row(
+                 mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    if (showFilterIcon.isTrue) ...[
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 18),
+                        child: IconButton(
+                            onPressed: () async {
+                              showFilterIcon.value = false;
+                            },
+                            icon: const Icon(
+                              Icons.filter_list_alt,
+                              color: Colors.white,
+                            )),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 18),
+                        child: IconButton(
+                            onPressed: () async {
+                              QuickAlert.show(
+                                context: context,
+                                type: QuickAlertType.confirm,
+                                title: '',
+                                cancelBtnText: 'No',
+                                confirmBtnText: 'yes',
+                                text:
+                                    "Are you sure you want to log out? Your session will be terminated and you will need to log in again to use the app.",
+                                onConfirmBtnTap: () {
+                                  AuthController.instance.logout();
+                                  Get.to(() => const LoginScreen());
+                                },
+                              );
+                            },
+                            icon: const Icon(
+                              Icons.logout,
+                              color: Colors.white,
+                            )),
+                      ),
+                    ] else ...[
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: filterView()),
+                    ],
+                  ],
                 ),
               ),
               Obx(
@@ -85,7 +111,7 @@ class _GymOwnerHomeScreenState extends State<GymOwnerHomeScreen> {
                               children: [
                                 Row(
                                   children: [
-                                    buildText(content: 'User name: '),
+                                    buildText(content: 'name: '),
                                     buildText(
                                         content:
                                             '${doc['isUserSignedOutForDay']}',
@@ -96,27 +122,33 @@ class _GymOwnerHomeScreenState extends State<GymOwnerHomeScreen> {
                                   children: [
                                     buildText(content: 'Date: '),
                                     buildText(
-                                        content:
-                                        '${doc['date']}',
+                                        content: '${doc['date']}',
                                         isContentStyleView: true),
                                   ],
                                 ),
                                 Row(
                                   children: [
-                                    buildText(content: 'Sign-in time: '),
-                                    buildText(
-                                        content:
-                                        '${doc['signInTime']}',
-                                        isContentStyleView: true),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    buildText(content: 'Sign-out time: '),
-                                    buildText(
-                                        content:
-                                        '${doc['signOutTime']}',
-                                        isContentStyleView: true),
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 20),
+                                      child: Column(
+                                        children: [
+                                          buildText(content: 'SignedIn '),
+                                          buildText(
+                                              content: '${doc['signInTime']}',
+                                              isContentStyleView: true),
+
+                                        ],
+                                      ),
+                                    ),
+                                    Column(
+                                      children: [
+                                        buildText(content: 'SignedOut '),
+
+                                        buildText(
+                                            content: '${doc['signOutTime']}',
+                                            isContentStyleView: true),
+                                      ],
+                                    )
                                   ],
                                 ),
                               ],
@@ -142,8 +174,57 @@ class _GymOwnerHomeScreenState extends State<GymOwnerHomeScreen> {
         content,
         style: isContentStyleView
             ? const TextStyle(color: Colors.white)
-            : const TextStyle(color: Colors.white, fontWeight: FontWeight.bold,fontSize: 15),
+            : const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15),
       ),
     );
+  }
+
+  Widget filterView() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () {},
+              child: filterButtonView('name'),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: filterButtonView('date'),
+            ),
+            IconButton(
+                onPressed: () async {
+                  showFilterIcon.value = true;
+                },
+                icon: const Icon(
+                  Icons.close,
+                  color: Colors.white,
+                )),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Container filterButtonView(String buttonName) {
+    return Container(
+              decoration: BoxDecoration(
+                color: /* selected ?*/ Colors.blue /*: Colors.white*/,
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.white),
+              ),
+              margin: const EdgeInsets.only(bottom: 16,top: 16,right: 8,left: 8),
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: Text(
+                buttonName,
+                style: const TextStyle(
+                  color: /*selected ?*/ Colors.white,
+                  /*: Colors.blue,*/
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
   }
 }
